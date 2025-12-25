c2f6dd08b01a5e5670a7f47739e08590040855e7
girishsuthar229 <girishgajjar2003@gmail.com>
2025-12-25 12:01:10 +0530

    update the next config

--- next.config.ts
+++ next.config.ts
@@ -6,17 +6,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname:
-          process.env.NEXT_PUBLIC_HOST ||
-          "social-media-application-backend-y8zs.onrender.com",
-        port: process.env.NEXT_PUBLIC_PORT || "5483",
-        pathname: "/uploads/**",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
-    domains: [
-      process.env.NEXT_PUBLIC_HOST ||
-        "social-media-application-backend-y8zs.onrender.com",
-    ],
+    domains: ["res.cloudinary.com"],
   },
   webpack: (config) => {
     // Disable webpack filesystem cache to avoid serialization warnings
