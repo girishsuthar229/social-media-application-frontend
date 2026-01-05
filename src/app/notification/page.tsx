0b5e87bc2ce28a8967feee19bb5bba5fbcefe622
girishsuthar229 <girishgajjar2003@gmail.com>
2026-01-05 10:55:39 +0530

    added the new notification user web socket integration

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, List, Typography } from "@mui/material";
 import { UserPlus } from "lucide-react";
 import { toast } from "react-toastify";
 import { IApiError } from "@/models/common.interface";
@@ -13,7 +13,9 @@ import RequestItem from "./components/requestItem";
 import useSocket from "@/util/socket";
 import { useRouter } from "next/navigation";
 import BackButton from "@/components/common/BackButton";
-import { NewUserNotification } from "@/models/userInterface";
+import { NewUserNotification, UserAllListModel } from "@/models/userInterface";
+import { getAllUsers } from "@/services/user-service.service";
+import UserlistWithFollowBtn from "@/components/common/UserlistWithFollow/UserlistWithFollowBtn";
 
 const Notification = () => {
   const { currentUser } = UseUserContext();
@@ -23,11 +25,35 @@ const Notification = () => {
   const [newUsers, setNewUsers] = useState<NewUserNotification[]>([]);
   const socket = useSocket(currentUser?.id.toString());
   const router = useRouter();
+  const [showSuggestedUsers, setShowSuggestedUsers] = useState(false);
+  const [suggestedUsers, setAllSuggestedUsers] = useState<UserAllListModel[]>(
+    []
+  );
+  const [showSuggestedUsersHasMore, setShowSuggestedUsersHasMore] =
+    useState(true);
+  const [allUsersTotalCount, setAllUsersTotalCount] = useState<number>();
+  const [userOffset, setUserOffset] = useState<number>(0);
+  const [userLoading, setUserLoading] = useState(false);
 
   useEffect(() => {
     if (!socket) return;
-    socket.on("new_user_created", (user) => {
-      setNewUsers((prev) => [...prev, user]);
+    socket.on("new_user_created", (user: any) => {
+      const newUser: NewUserNotification = {
+        id: user?.id,
+        user_name: user?.user_name,
+        first_name: user?.first_name || null,
+        last_name: user?.last_name || null,
+        bio: user?.bio || null,
+        photo_url: user?.photo_url || null,
+      };
+
+      setNewUsers((prevUsers) => {
+        const updatedUsers = [newUser, ...prevUsers];
+        if (updatedUsers.length > 4) {
+          updatedUsers.pop();
+        }
+        return updatedUsers;
+      });
     });
 
     return () => {
@@ -42,9 +68,59 @@ const Notification = () => {
   useEffect(() => {
     if (currentUser?.id) {
       loadPendingRequests();
+      loadMoreUsers();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUser?.id]);
+
+  const loadMoreUsers = async (sgtUser?: boolean) => {
+    if (userLoading || !showSuggestedUsersHasMore) return;
+    setUserLoading(true);
+    const payload = {
+      limit: 10,
+      offset: userOffset,
+      sortBy: "created_date",
+      sortOrder: "DESC",
+    };
+
+    try {
+      const response = await getAllUsers(payload);
+      if (response?.data && response.statusCode === STATUS_CODES.success) {
+        const newUsers: UserAllListModel[] = response.data?.rows || [];
+        if (showSuggestedUsers || sgtUser) {
+          setAllSuggestedUsers((prev) => [...prev, ...newUsers]);
+          setAllUsersTotalCount(response.data?.count);
+          setUserOffset((prev) => prev + newUsers.length);
+          if (newUsers.length < 10) {
+            setShowSuggestedUsersHasMore(false);
+          }
+        }
+        if (!showSuggestedUsers) {
+          const newUserNotifications: NewUserNotification[] = newUsers
+            .slice(0, 4)
+            .map((user) => ({
+              id: user?.id,
+              user_name: user?.user_name,
+              first_name: user?.first_name || null,
+              last_name: user?.last_name || null,
+              bio: user?.bio || null,
+              photo_url: user?.photo_url || null,
+            }));
+          setNewUsers((prev) => [...prev, ...newUserNotifications]);
+        }
+      }
+    } catch (error) {
+      const err = error as IApiError;
+      toast.error(err?.message);
+    } finally {
+      setUserLoading(false);
+    }
+  };
+
+  const handleMoreSuggestedUsers = () => {
+    setShowSuggestedUsers(true);
+    loadMoreUsers(true);
+  };
 
   const loadPendingRequests = async () => {
     if (!currentUser?.id) return;
@@ -96,32 +172,89 @@ const Notification = () => {
       >
         <Box className="notification-header">
           <Typography variant="h5" className="page-title">
-            Highlights
+            Suggested Users
           </Typography>
         </Box>
-        <Box className="new-users-list">
-          {/* List of new users */}
-          {newUsers.length > 0 ? (
-            newUsers.map((user) => (
-              <Box key={user.id} className="new-users-message">
+        {!showSuggestedUsers && (
+          <Box className="new-users-list">
+            {/* List of new users */}
+            {newUsers.length > 0 ? (
+              <Box className="new-users-message">
                 <Typography variant="body2">
-                  New user created, please follow them!{" "}
-                  <BackButton
-                    onClick={() => handleUserClick(user.user_name)}
-                    labelText={`@ ${user.user_name}`}
-                    showIcon={false}
-                    underlineOnHover={true}
-                  />
+                  New user(s) created, please follow them!{" "}
+                </Typography>
+                {newUsers.map((user, index) => (
+                  <Typography
+                    component={"span"}
+                    key={user?.id || index}
+                    className="user-name-container"
+                    onClick={() => handleUserClick(user?.user_name)}
+                  >
+                    <Typography component={"span"} className="user-name">
+                      {`@${user?.user_name}`}
+                      {" , "}
+                    </Typography>
+                  </Typography>
+                ))}
+                <Typography
+                  component="span"
+                  className="more-users-message"
+                  onClick={() => handleMoreSuggestedUsers()}
+                >
+                  {" more"}
                 </Typography>
               </Box>
-            ))
-          ) : (
-            <Box className="new-users-message">
-              <Typography variant="body2">No Highlights</Typography>
-            </Box>
-          )}
-        </Box>
-
+            ) : (
+              <Box className="new-users-message">
+                <Typography variant="body2">No Highlights</Typography>
+              </Box>
+            )}
+          </Box>
+        )}
+        {showSuggestedUsers && (
+          <Box className="suggested-user-div notofication-sgstd-user-div">
+            <List className="scrollbar">
+              {suggestedUsers.map((user: UserAllListModel, index: number) => (
+                <Box key={index}>
+                  <UserlistWithFollowBtn
+                    user={{
+                      id: user?.id,
+                      user_name: user?.user_name,
+                      first_name: user?.first_name,
+                      last_name: user?.last_name,
+                      photo_url: user?.photo_url,
+                      bio: user?.bio || null,
+                      is_following: user?.is_following,
+                      follow_status: user?.follow_status,
+                    }}
+                    showBio={true}
+                    showFullName={true}
+                    showFollowButton={true}
+                    currentUser={currentUser}
+                  />
+                </Box>
+              ))}
+              {userLoading ? (
+                <UserListSkeleton
+                  count={3}
+                  showBio={true}
+                  showFollowButton={true}
+                />
+              ) : (
+                Number(suggestedUsers.length) < Number(allUsersTotalCount) && (
+                  <div className="load-more-div">
+                    <BackButton
+                      onClick={!loading ? loadMoreUsers : undefined}
+                      labelText={loading ? "Loading..." : "Load More"}
+                      showIcon={false}
+                      underlineOnHover={true}
+                    />
+                  </div>
+                )
+              )}
+            </List>
+          </Box>
+        )}
         <Box className="notification-header">
           <Typography variant="h5" className="page-title">
             Notifications
